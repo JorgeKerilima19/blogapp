354bafe9bb42248cbd58e9fdedbe95702353b94f
JorgeMioska <quispechilojorge@gmail.com>
2023-10-21 00:43:56 -0500

    project basic set up

---
+++ front/src/main.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = document.getElementById("app");
+
+ReactDOM.createRoot(root as HTMLElement).render(<h1>Hello World</h1>);
