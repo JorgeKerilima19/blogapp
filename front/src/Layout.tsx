3188c14a99ae6f68214510c032c67fcc2c99e767
JorgeMioska <quispechilojorge@gmail.com>
2023-10-23 21:35:38 -0500

    tailwind and navbar component

--- front/src/Layout.tsx
+++ front/src/Layout.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./components";
 
+import "./style.css";
+
 const Layout = () => {
   return (
     <>
-      <header>
+      <header className="bg-slate-600 text-white px-5 h-12 grid items-center">
         <Navbar />
       </header>
       <main>
--- front/src/components/Navbar.tsx
+++ front/src/components/Navbar.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex justify-between">
+      <Link to={"/"}>
+        <img src="" alt="logo" />
+      </Link>
+      <ul className="flex gap-5">
+        <Link to={"/register"}>Log In</Link>
+        <Link to={"/main"}>Main</Link>
+      </ul>
+    </nav>
+  );
 };
