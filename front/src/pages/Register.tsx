8bf48b8bfade826bc4cc7e493ab05a634014cdb6
JorgeMioska <quispechilojorge@gmail.com>
2023-10-24 08:05:07 -0500

    register form styles

--- front/src/pages/Register.tsx
+++ front/src/pages/Register.tsx
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Register = () => {
-  return <div>Register</div>;
+  const [userName, setUserName] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  return (
+    <section className="min-h-screen grid place-items-center">
+      <form
+        action=""
+        className="grid place-items-center gap-8"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <h2 className="text-2xl">Register</h2>
+        <input
+          className={
+            "py-1 px-2 border-2 border-gray-400 focus:border-black transition duration-300"
+          }
+          type="text"
+          value={userName}
+          placeholder="type smth"
+          onChange={(e) => {
+            setUserName(e.target.value);
+          }}
+        />
+        <input
+          className={
+            "py-1 px-2 border-2 border-gray-400 focus:border-black transition duration-300"
+          }
+          type="password"
+          value={password}
+          placeholder="type smth"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button type="submit" className="py-2 px-5 bg-gray-500">
+          Sign Up
+        </button>
+      </form>
+    </section>
+  );
 };
